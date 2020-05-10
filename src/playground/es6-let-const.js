3a55822dba8eaa9ed4eb9b93a0dee5807277a813
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-10 13:55:51 -0400

    200510-1355 - as of this moment

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,29 +1,28 @@
-'use strict';
-
 var nameVar = 'igoryen';
 var nameVar = 'igor';
 console.log('nameVar: ', nameVar);
 
-var nameLet = 'Jenica';
+let nameLet = 'Jenica';
 nameLet = 'Joy';
 console.log('nameLet: ', nameLet);
 
-var nameConst = 'Frank';
+
+const nameConst = 'Frank';
 // nameConst = 'Daniel';
 console.log('nameConst: ', nameConst);
 
 function getPetName() {
-    var petName = 'Hank';
+    const petName = 'Hank';
     return petName;
 }
-var petName = getPetName();
+const petName = getPetName();
 getPetName();
 console.log(petName);
 
 var fullName = 'Igor Yen';
-if (fullName) {
-    var _firstName = fullName.split(' ')[0];
-    console.log(_firstName);
-}
+if( fullName ) {
+    const firstName = fullName.split(' ')[0];
+    console.log(firstName)
+} 
 
 console.log(firstName);
