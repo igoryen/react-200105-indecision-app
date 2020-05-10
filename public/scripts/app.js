04d8ac773726d5ef930b40bd0b646e777e7f4367
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-10 14:38:07 -0400

    200510-1437 - ran 'babel ./src/app.js --out-file=public/scripts/app.js --presets=env,react'

---
+++ public/scripts/app.js
@@ -0,0 +1,80 @@
+"use strict";
+
+var app = {
+    title: "Some title",
+    subtitle: " Some subtitle",
+    options: ['One', 'Two']
+};
+
+// JSX - Javascript XML
+var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        "p",
+        null,
+        app.subtitle
+    ),
+    app.options && app.options.length > 0 && React.createElement(
+        "p",
+        null,
+        "Here are your options:"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "item 1"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "item 2"
+        )
+    )
+);
+
+var user = {
+    name: 'Igor Yen',
+    age: 45,
+    location: 'Earth'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            "p",
+            null,
+            "Location: ",
+            location
+        );
+    }
+}
+
+var template2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    getLocation(user.location)
+);
+
+var appRoot = document.getElementById("app");
+// ReactDOM.render(template2, appRoot);
+ReactDOM.render(template, appRoot);
