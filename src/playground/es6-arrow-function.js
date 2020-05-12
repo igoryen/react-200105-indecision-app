96cf0fffcf6c8bb6277c18e5384a780c756dc450
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-12 14:27:18 -0400

    14-a. ES6 Aside: Arrow Functions

--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,16 +1,14 @@
-"use strict";
-
-var square = function square(x) {
-    // (10)
+const square = function (x) { // (10)
     return x * x;
-};
+}
 console.log(square(8));
 
-var squareArrow = function squareArrow(x) {
-    // (20)
+
+const squareArrow = (x) => { // (20)
     return x * x;
-};
+}
 console.log(squareArrow(9));
+
 
 /**
  * 10 - ES5 function. Can have a name.
