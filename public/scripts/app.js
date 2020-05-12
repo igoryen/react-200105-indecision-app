b09f9a3fe7d4ccf5580f6631c87b6687758d2a50
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-12 14:34:12 -0400

    14-b. ES6 Aside: Arrow Functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,13 +6,18 @@ var square = function square(x) {
 };
 console.log(square(8));
 
+// const squareArrow = (x) => { // (20)
+//     return x * x;
+// }
+
 var squareArrow = function squareArrow(x) {
-    // (20)
     return x * x;
-};
+}; // (30)
+
 console.log(squareArrow(9));
 
 /**
  * 10 - ES5 function. Can have a name.
  * 20 - ES6 function. Always anonymous. Cannot add a name. Good for conciseness.
+ * 30 - an even more concise version of (20)
  */
--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -4,13 +4,17 @@ const square = function (x) { // (10)
 console.log(square(8));
 
 
-const squareArrow = (x) => { // (20)
-    return x * x;
-}
+// const squareArrow = (x) => { // (20)
+//     return x * x;
+// }
+
+const squareArrow = (x) => x * x; // (30)
+
 console.log(squareArrow(9));
 
 
 /**
  * 10 - ES5 function. Can have a name.
  * 20 - ES6 function. Always anonymous. Cannot add a name. Good for conciseness.
+ * 30 - an even more concise version of (20)
  */
