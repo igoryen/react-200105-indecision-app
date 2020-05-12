4bca12eaa256eebe7b6c191aae226448f9cc3b7d
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-12 14:42:43 -0400

    14-c. ES6 Aside: Arrow Functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,20 +1,36 @@
-"use strict";
+'use strict';
 
-var square = function square(x) {
-    // (10)
-    return x * x;
-};
-console.log(square(8));
-
-// const squareArrow = (x) => { // (20)
+// const square = function (x) { // (10)
 //     return x * x;
 // }
+// console.log(square(8));
 
-var squareArrow = function squareArrow(x) {
-    return x * x;
-}; // (30)
 
-console.log(squareArrow(9));
+// // const squareArrow = (x) => { // (20)
+// //     return x * x;
+// // }
+
+// const squareArrow = (x) => x * x; // (30)
+
+// console.log(squareArrow(9));
+
+
+//===============================
+// 2 versions of the same function: expaned and short
+// getFirstName('Mike') outputs 'Mike'
+
+var fullName = 'Andrew Mead';
+
+var getFirstNameA = function getFirstNameA(fullName) {
+    return fullName.split(' ')[0];
+};
+
+var getFirstNameB = function getFirstNameB(fullName) {
+    return fullName.split(' ')[0];
+};
+
+console.log("getFirstNameA", getFirstNameA(fullName));
+console.log("getFirstNameB", getFirstNameB(fullName));
 
 /**
  * 10 - ES5 function. Can have a name.
--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,16 +1,32 @@
-const square = function (x) { // (10)
-    return x * x;
-}
-console.log(square(8));
-
-
-// const squareArrow = (x) => { // (20)
+// const square = function (x) { // (10)
 //     return x * x;
 // }
+// console.log(square(8));
 
-const squareArrow = (x) => x * x; // (30)
 
-console.log(squareArrow(9));
+// // const squareArrow = (x) => { // (20)
+// //     return x * x;
+// // }
+
+// const squareArrow = (x) => x * x; // (30)
+
+// console.log(squareArrow(9));
+
+
+//===============================
+// 2 versions of the same function: expaned and short
+// getFirstName('Mike') outputs 'Mike'
+
+const fullName = 'Andrew Mead'; 
+
+const getFirstNameA = (fullName) => {
+    return fullName.split(' ')[0];
+}
+
+const getFirstNameB = (fullName) => fullName.split(' ')[0];
+
+console.log("getFirstNameA", getFirstNameA(fullName));
+console.log("getFirstNameB", getFirstNameB(fullName));
 
 
 /**
