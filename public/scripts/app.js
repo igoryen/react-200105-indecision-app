db76f14d130862c4ad7a6fcd906f8f38b3f2f22b
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-14 14:12:54 -0400

    24-d. ES6 classes. Part 1. Constructor with default val.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,12 +5,14 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 var Person = function Person(name) {
     _classCallCheck(this, Person);
 
-    this.name = name; // (10)
+    this.name = name || 'default'; // (10)
 };
 
 var me = new Person('Andrew Mead');
-
 console.log(me);
+
+var other = new Person();
+console.log(other);
 
 /**
  * 10 - 'this' refers to the instance of the class.
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,12 +1,14 @@
 class Person {
     constructor(name) {
-        this.name = name;// (10)
+        this.name = name || 'default';// (10)
     }
 }
 
 const me = new Person('Andrew Mead');
-
 console.log(me);
+
+const other = new Person();
+console.log(other);
 
 /**
  * 10 - 'this' refers to the instance of the class.
