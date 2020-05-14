d2a144f42c81f2baef21fc0d478f5eda8b87ced8
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-14 14:04:06 -0400

    24-a. ES6 classes. Part 1.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,36 +1,11 @@
-'use strict';
+"use strict";
 
-var viz = false; // viz = visibility
-var toggleViz = function toggleViz() {
-    viz = !viz;
-    render();
-};
-var render = function render() {
-    var btnlbl = (viz ? 'Hide' : 'Show') + ' details';
-    var jsx = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Visibility Toggle'
-        ),
-        React.createElement(
-            'button',
-            { onClick: toggleViz },
-            btnlbl
-        ),
-        viz && React.createElement(
-            'div',
-            null,
-            React.createElement(
-                'p',
-                null,
-                'Hey, these are some details you can now see!'
-            )
-        )
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Person = function Person() {
+  _classCallCheck(this, Person);
 };
 
-render();
+var me = new Person();
+
+console.log(me);
