a3e43104e6f8e7f948909c8753657c00d9014fe4
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-14 14:17:55 -0400

    24-F. ES6 classes. Part 1. Class method.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,20 +1,33 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Person = function Person() {
-    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
 
-    _classCallCheck(this, Person);
+        _classCallCheck(this, Person);
 
-    this.name = name; // (10)
-};
+        this.name = name; // (10)
+    }
+
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi!';
+        }
+    }]);
+
+    return Person;
+}();
 
 var me = new Person('Andrew Mead');
-console.log(me);
+console.log(me.getGreeting());
 
 var other = new Person();
-console.log(other);
+console.log(other.getGreeting());
 
 /**
  * 10 - 'this' refers to the instance of the class.
--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -2,13 +2,16 @@ class Person {
     constructor(name = 'Anonymous') {
         this.name = name;// (10)
     }
+    getGreeting(){
+        return 'Hi!';
+    }
 }
 
 const me = new Person('Andrew Mead');
-console.log(me);
+console.log(me.getGreeting());
 
 const other = new Person();
-console.log(other);
+console.log(other.getGreeting());
 
 /**
  * 10 - 'this' refers to the instance of the class.
