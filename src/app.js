b3629eebee13c8e5935896fe6aa677ee635477b0
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-14 17:17:17 -0400

    26-A. CR8 a React comp. Basic.

---
+++ src/app.js
@@ -0,0 +1,16 @@
+class Header extends React.Component {
+    render() {
+        return <p>This is from Header</p>
+    }
+}
+
+const jsx = (
+    <div>
+        <h1>Title</h1>
+        <Header/>
+        <Header/>
+        <Header/>
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
