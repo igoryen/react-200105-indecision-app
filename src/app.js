347f7f9116e8668109cb154491f82ffe0d8659ee
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-14 17:28:33 -0400

    26-B. CR8 a React comp. Action added.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,28 @@
 class Header extends React.Component {
     render() {
-        return <p>This is from Header</p>
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        )
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
     }
 }
 
 const jsx = (
     <div>
-        <h1>Title</h1>
         <Header/>
-        <Header/>
-        <Header/>
+        <Action/>
     </div>
 );
 
