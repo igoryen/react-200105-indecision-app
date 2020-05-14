b6628b5b51816cd42e686f80bee653eed434ef1a
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-14 17:11:54 -0400

    prep for next

---
+++ src/playground/jsx-indecision.js
@@ -0,0 +1,79 @@
+const app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer",
+    options: []
+};
+
+const onFormSubmit = (e) => {
+    e.preventDefault(); // (10)
+    const option = e.target.elements.option.value; // (20)
+    if(option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderApp();
+    }
+};
+
+const wipeAll = () => {
+    app.options = [];
+    renderApp();
+};
+
+const user = {
+    name: 'Igor Yen',
+    age: 45,
+    location: 'Earth'
+};
+
+function getLocation( location ) {
+    if(location){
+        return <p>Location: {location}</p>;
+    }    
+}
+
+const template2 = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div> 
+);
+
+// D6D = decide
+const onD6D = () => {
+    const rno = Math.floor(Math.random() * app.options.length); // rno = random number
+    const opt = app.options[rno];
+    alert(opt);
+};
+
+const appRoot = document.getElementById("app");
+
+const renderApp = () => {
+    // JSX - Javascript XML
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p> }
+            {(app.options && app.options.length > 0) && <p>Here are your options:</p>}
+            <button disabled={app.options.length === 0} onClick={onD6D}>What should I do?</button>
+            <button onClick={wipeAll}>Remove ALL</button>
+            <ol>
+                {
+                    app.options.map((opt) => <li key={opt}>{opt}</li>)
+                }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    ); 
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp();
+
+/**
+ * 10 - Stop the full-page refresh so that other code below can be run. 
+ * 20 - element on which the event was fired, here <form>
+ */
