7bad31b5e123d90f455d81fd0d278d0c01c5511f
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-17 15:21:13 -0400

    34-C. Alternative setState Syntax. Cleanup.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,11 +51,6 @@ var Counter = function (_React$Component) {
                     count: 0
                 };
             });
-            this.setState(function (prevState) {
-                return {
-                    count: prevState.count + 1
-                };
-            });
         }
     }, {
         key: 'render',
