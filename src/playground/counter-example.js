93c3c8d448255a18ac1f3d90e1d5c4101c4749f2
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-05-17 13:47:06 -0400

    32. Adding State to Counter App: Part I. Added constructor and 3 methods.

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,11 +1,31 @@
 class Counter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
+
+    handleAddOne() {
+        console.log('handleAddOne');
+    }
+
+    handleMinusOne() {
+        console.log('handleMinusOne');
+    }
+
+    handleReset() {
+        console.log('handleReset');
+    }
+
     render() {
         return (
             <div>
                 <h1>Count: </h1>
-                <button>+1</button>
-                <button>-1</button>
-                <button>Reset</button>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>Reset</button>
             </div>
         );
     }
