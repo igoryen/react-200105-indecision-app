0c18e93d3de1a8dcec2796e0625b028ba8043703
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-02 14:22:45 -0400

    51-B. ES6 import/export. Named exports together.

--- public/bundle.js
+++ public/bundle.js
@@ -70,16 +70,28 @@
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__utils_js__);
+// import './utils.js'
 
 
 console.log('app.js is running!!!');
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["b" /* square */](4));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["a" /* add */](100,23));
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return square; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return add; });
 console.log('utils.js is running!!!');
+
+const square = (x) => x * x;
+
+const add = (a, b) => a + b;
+
+// named export
+
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
-import './utils.js'
+// import './utils.js'
+import { square, add } from './utils.js';
 
 console.log('app.js is running!!!');
+console.log(square(4));
+console.log(add(100,23));
