cb614cf4e3ec5baf8a6c0317d973bb5532d0b4f3
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-02 14:33:49 -0400

    51-D. ES6 import/export. Another file person.js.

--- public/bundle.js
+++ public/bundle.js
@@ -69,31 +69,30 @@
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js__ = __webpack_require__(1);
-// import './utils.js'
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
+// // import './utils.js'
+// import { square, add } from './utils.js';
+
+// console.log('app.js is running!!!');
+// console.log(square(4));
+// console.log(add(100,23));
 
 
-console.log('app.js is running!!!');
-console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["b" /* square */](4));
-console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["a" /* add */](100,23));
+console.log("is adult? ", __WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](20));
+console.log("can drink?", __WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](21));
 
 /***/ }),
-/* 1 */
+/* 1 */,
+/* 2 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-console.log('utils.js is running!!!');
-
-const square = (x) => x * x;
-/* harmony export (immutable) */ __webpack_exports__["b"] = square;
-
-
-const add = (a, b) => a + b;
-/* harmony export (immutable) */ __webpack_exports__["a"] = add;
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+const isAdult = (age) => age >= 18;
+const canDrink = (age) => age >= 21;
 
 
-// named export
-// export { square, add };
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-// import './utils.js'
-import { square, add } from './utils.js';
+// // import './utils.js'
+// import { square, add } from './utils.js';
 
-console.log('app.js is running!!!');
-console.log(square(4));
-console.log(add(100,23));
+// console.log('app.js is running!!!');
+// console.log(square(4));
+// console.log(add(100,23));
+
+import { isAdult, canDrink} from './person.js';
+console.log("is adult? ", isAdult(20));
+console.log("can drink?", canDrink(21));
