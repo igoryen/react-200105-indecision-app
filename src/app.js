5cbb2b14d81999f0804767b1731fceff167e8e2e
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-07 16:28:09 -0400

    55-C. One Component per File. Factored out AddOption into a separate file.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // install -> import -> use
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AddOption from './components/AddOption';
 
 class IndecisionApp extends React.Component {
     constructor(props) {
@@ -153,35 +154,5 @@ const Option = (props) => {
         </div>
     )
 };
-
-class AddOption extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.state = {
-            error: undefined
-        }
-    }
-    handleAddOption(e) {
-        e.preventDefault();
-        const option = e.target.elements.opt.value.trim();
-        const error = this.props.handleAddOption(option);
-        this.setState(() => ({ error }));
-        if (!error) {
-            e.target.elements.opt.value = ''; // clear text input field
-        }
-    }
-    render() {
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.handleAddOption}>
-                    <input type="text" name="opt" />
-                    <button>Add Option</button>
-                </form>
-            </div>
-        );
-    }
-}
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
